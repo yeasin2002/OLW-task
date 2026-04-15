2b38c6abcc81c6f682a90b53c0850d348570b961
Md Kawsar Islam Yeasin <87494463+yeasin2002@users.noreply.github.com>
2026-04-15 14:21:17 +0600

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,43 @@
+import bundleAnalyzer from '@next/bundle-analyzer'
+import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const withNextIntl = createNextIntlPlugin()
+const withBundleAnalyzer = bundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })
+
+const nextConfig: NextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+
+  experimental: {
+    typedRoutes: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: process.env.NODE_ENV === 'production',
+  },
+
+  typescript: {
+    ignoreBuildErrors: process.env.NODE_ENV === 'production',
+  },
+
+  images: {
+    remotePatterns: [],
+  },
+}
+
+export default withBundleAnalyzer(withNextIntl(nextConfig))
